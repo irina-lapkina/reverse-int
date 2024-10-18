9f40f923dd9cdcd79aa62e2805f9d6eed465d7f2
irina-lapkina <infokotostudio@gmail.com>
2024-10-18 14:20:05 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    const absN = Math.abs(n);
+    const stringN = absN.toString();
+    const arr = stringN.split(''); 
+    const reversed = arr.reverse(); 
+    const reversedString = reversed.join('');
+    return +(reversedString);
+    }
